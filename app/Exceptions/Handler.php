3750af50e62203eeb0803a4a3932508be12c436a
teguhmuhams <teguhmuhams@gmail.com>
2023-11-23 07:23:43 +0700

    restore handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,6 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -28,64 +26,5 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
-    }
-
-    public function render($request, Throwable $e): JsonResponse
-    {
-        // This will replace the default Laravel exception page with a JSON response containing the error details
-        if ($request->expectsJson()) {
-            return $this->handleApiException($request, $e);
-        }
-
-        return parent::render($request, $e);
-    }
-
-    // public function render($request, Throwable $e): JsonResponse
-    // {
-    //     // If the request wants JSON (AJAX/Api call)
-    //     if ($request->expectsJson()) {
-    //         return response()->json([
-    //             'message' => $e->getMessage(),
-    //             'status'  => $e->getCode(),
-    //         ]);
-    //     }
-
-    // Default to the parent class' implementation of handler
-
-
-    private function handleApiException(Request $request, Throwable $exception)
-    {
-        $exception = $this->prepareException($exception);
-
-        if ($exception instanceof \Illuminate\Http\Exceptions\HttpResponseException) {
-            return $exception->getResponse();
-        }
-
-        if ($exception instanceof \Illuminate\Auth\AuthenticationException) {
-            return $this->unauthenticated($request, $exception);
-        }
-
-        if ($exception instanceof \Illuminate\Validation\ValidationException) {
-            return $this->convertValidationExceptionToResponse($exception, $request);
-        }
-
-        // Define the response elements
-        $statusCode = 500;
-        if (method_exists($exception, 'getStatusCode')) {
-            $statusCode = $exception->getStatusCode();
-        }
-
-        $response = [];
-
-        $response['message'] = $exception->getMessage();
-
-        if (config('app.debug')) {
-            $response['exception'] = get_class($exception);
-            $response['file'] = $exception->getFile();
-            $response['line'] = $exception->getLine();
-            $response['trace'] = $exception->getTrace();
-        }
-
-        return response()->json($response, $statusCode);
     }
 }
